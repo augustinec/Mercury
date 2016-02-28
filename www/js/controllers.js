5e249f1b876b24223162f8d83e05f9db78e2ba30
augustinec <achirombo@gmail.com>
2016-02-28 14:17:13 +0200

    Fixed the request URL and returns the JSON response which is  not yet binded to the view.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,5 @@
 angular.module('mercury.controllers', ['ionic'])
 
-.controller('AppStart', function($scope, $state, $timeout, $ionicLoading, $ionicHistory ){
-	//user should not be able to Navigate back to the welcome screen 
-	$ionicHistory.nextViewOptions({ 
-		disableBack: true
-	});
-	//use a welcome screen to the application
-	$timeout(function(){ 
-		$scope.loading = $ionicLoading.show();
-		console.log('Application Mecury Fired Up');
-	}, 2000);	
-	$timeout(function(){ 
-		$ionicLoading.hide();		 
-		$state.go('abstract.home');
-	}, 4000); 	
-	//use timer to navigate to the home from the welcome 
-})
-
 .controller('AbstractCtrl', function($scope, $ionicSideMenuDelegate){
 })
 
@@ -24,59 +7,47 @@ angular.module('mercury.controllers', ['ionic'])
 	//ensure that the back button or function closes the App
 	$ionicPlatform.registerBackButtonAction(function(){
 		var path = $location.path(); 
-		if($location.path() === "/home" || $location.path() === "home"){
-			navigator.app.exitApp(); 
+		if($location.path() == "/abstract/home" || $location.path() == "abstract/home"){
+			ionic.Platform.exitApp(); //close the application
 		}
 		else{
 			$ionicHistory.goBack(); 
 		}
 	}, 100);
-	$scope.loading = $ionicLoading.show();
-	//removed actual values of access and secrete key 
-	var access_key = 'Lg7ywecDYG119uTJrlhLSH6X65qm12BOqCeTt7X2';
-	var secrete_key = 'Acf1HHz41AJcGdpDEZUgIlWJ3u3Ik71tzQLRIvB3'; 
-	var tonce = Math.round(new Date().getTime()/1000.0); 
-	var payload = 'GET|/api/v1/markets|access_key=' + access_key + '&foo=bar&tonce=' + tonce;
-	
-	//compute signature from the secrete key and payload. ( NB:use HMAC SHA-256 )
-	var hashType = 'SHA-256'; 
-	var shaObj = new jsSHA(hashType, "TEXT");
-	shaObj.setHMACKey(access_key, "TEXT");
-	shaObj.update(payload);
-	var signature = shaObj.getHMAC("HEX");	
-	console.log(signature); 
 	
 	$scope.data = {
-		currency: 'USD', 
-		amount: '0.00', 
-		err: ''
+		//variable to set the currency 
+		currency: 'USD'
 	};
 	//valid request needs three authentication parts : access key, signature, server time stamp(Tonce)
-	var request_URL = 'https://bitcoinfundi.com/api/v1/markets?access_key=' + access_key + '&foo=bar&tonce=' + tonce + '&signature=' + signature; 
 	
-	$http({method: 'GET', url: request_URL, cache: $templateCache}).
-		then(function(response) {
-			$scope.data.amount = response.data.ticker.buy;
-			$scope.status = response.status;
-			
-		}, function(response) {
-			var alertPopup = $ionicPopup.alert({
-				title: 'BitcoinFundi Api',
-				template: "Returned " + response.status || "Request failed " + response.status
-			});	
-			$scope.data.currency = 'USD'; 
-			$scope.data.amount = response.data.ticker.buy;
-			$scope.status = response.status;
-		});
-	$ionicLoading.hide();
-	/*$http.get('https://docsample.herokuapp.com/jsonSample').then(function(response){
-		//'response.data' will contain the result
-		//the JSON file returned with response should set the 
-		console.log('Successful');
-	}, function(err){
-		console.error('ERR', err); 
-		//err.status will contain the status code
-	}); */
+	//access values and variables 
+	var accessKey = "Lg7ywecDYG119uTJrlhLSH6X65qm12BOqCeTt7X2";
+	var secrete_key = "Acf1HHz41AJcGdpDEZUgIlWJ3u3Ik71tzQLRIvB3"; 
+	var t_stamp = Math.round(new Date().getTime()/1.0); //the tonce ( NB: server time stamp )
+	var payload = "GET|/api/v1/markets|access_key=" + accessKey + "&foo=bar&tonce=" + t_stamp;
+	
+	//compute signature from the secrete key and payload. ( NB:use HMAC SHA-256 )	
+	var shaObj = new jsSHA("SHA-256", "TEXT");
+	shaObj.setHMACKey(secrete_key, "TEXT");
+	shaObj.update(payload);
+	var hashOut = shaObj.getHMAC("HEX");	
+	
+	var request_URL =  "https://bitcoinfundi.com/api/v1/markets?access_key=" + accessKey + "&foo=bar&tonce=" + t_stamp + "&signature=" + hashOut; 
+	
+	
+	$http.get(request_URL).
+	then(function(response) {
+		console.log('Request successfull');	
+		//set the display values 
+		$scope.market = {
+			buy : response.data, 
+			sell : response.data, 
+			last : response.data
+		}; 
+	}, function(err) {
+		console.error('ERR', err); 			
+	});
 	
 	//launch the popover for the settings and information link
 	$ionicPopover.fromTemplateUrl('popover.html', {
@@ -98,21 +69,39 @@ angular.module('mercury.controllers', ['ionic'])
 	}; 
 	//reload the view to reload the values 
 	$scope.reloadData = function(){    
-		console.log('Refreshing!');
-		
+		console.log('Refreshing data !');		
 		$timeout( function() {
-			//simulate async response
-			$http({method: 'GET', url: request_URL, cache: $templateCache}).
+			//recompute the request
+			//access values and variables 
+			var accessKey = "Lg7ywecDYG119uTJrlhLSH6X65qm12BOqCeTt7X2";
+			var secrete_key = "Acf1HHz41AJcGdpDEZUgIlWJ3u3Ik71tzQLRIvB3"; 
+			var t_stamp = Math.round(new Date().getTime()/1.0); //the tonce ( NB: server time stamp )
+			var payload = "GET|/api/v1/markets|access_key=" + accessKey + "&foo=bar&tonce=" + t_stamp;
+
+			//compute signature from the secrete key and payload. ( NB:use HMAC SHA-256 )	
+			var shaObj = new jsSHA("SHA-256", "TEXT");
+			shaObj.setHMACKey(secrete_key, "TEXT");
+			shaObj.update(payload);
+			var hashOut = shaObj.getHMAC("HEX");	
+
+			var request_URL =  "https://bitcoinfundi.com/api/v1/markets?access_key=" + accessKey + "&foo=bar&tonce=" + t_stamp + "&signature=" + hashOut; 
+			$http.get(request_URL).
 			then(function(response) {
-				$scope.data.amount = response.data.ticker.buy;
-				$scope.status = response.status;
+				console.log('Request successfull');	
+				//set the display values
+				//use function to format the string JSON.parse(response); 
+				$scope.market = {
+					buy : response.data.id, 
+					sell : response.data, 
+					last : response.data
+				}; 
+				var alertPopup = $ionicPopup.alert({
+					title: 'New Prices',
+					template: 'Refresh complete.'
+				});
 				
 			}, function(err) {
-				var alertPopup = $ionicPopup.alert({
-					title: 'BitcoinFundi Api',
-					template: response.data.ticker.buy || "Request failed " + err.status
-				});	
-				$scope.err = err
+				console.error('ERR', err); 			
 			});
 
 			//Stop the ion-refresher from spinning
